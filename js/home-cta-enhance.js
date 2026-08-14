b5823f2bda4889823ceb7c5e7edc912620c899c7
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-15 08:44:30 +0900

    Refresh home project data for PTG LAW

--- js/home-cta-enhance.js
+++ js/home-cta-enhance.js
@@ -115,12 +115,12 @@
   loadStyle('./css/home-project-slider.css?v=20260807-2', 'home-project-slider-css');
 
   const projectScripts = [
-    ['./data/featured-projects.js?v=20260807-1', 'home-project-data-featured'],
-    ['./data/project-ptglaw.js?v=20260807-2', 'home-project-data-pentagon'],
+    ['./data/featured-projects.js?v=20260815-2', 'home-project-data-featured'],
+    ['./data/project-ptglaw.js?v=20260815-1', 'home-project-data-pentagon'],
     ['./data/project-haveaseat.js?v=20260806-1', 'home-project-data-haveaseat'],
     ['./data/project-relim.js?v=20260807-4', 'home-project-data-relim'],
     ['./data/project-jncostech.js?v=20260807-1', 'home-project-data-jncostech'],
-    ['./data/project-captures.js?v=20260806-5', 'home-project-captures-main'],
+    ['./data/project-captures.js?v=20260815-1', 'home-project-captures-main'],
     ['./data/project-haveaseat-captures.js?v=20260806-1', 'home-project-captures-haveaseat'],
     ['./data/project-relim-captures.js?v=20260807-1', 'home-project-captures-relim'],
     ['./data/project-pentagon-fallback.js?v=20260807-1', 'home-project-captures-pentagon'],
