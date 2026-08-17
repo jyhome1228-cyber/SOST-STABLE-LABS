10e29f6d5fb21391668d4437d3c2cfd1e6496e99
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-17 21:06:42 +0900

    Refresh compact larger header navigation styles

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@
   (async () => {
     await loadStyle('./css/project-unify.css?v=20260814-1', 'project-unify.css');
     await loadStyle('./css/blue-contrast-fix.css?v=20260817-1', 'blue-contrast-fix.css');
-    await loadStyle('./css/knowledge-nav.css?v=20260817-1', 'knowledge-nav.css');
+    await loadStyle('./css/knowledge-nav.css?v=20260817-2', 'knowledge-nav.css');
     await loadScript('./js/main-core.js?v=20260814-2', 'main-core');
     await loadScript('./js/knowledge-nav.js?v=20260817-1', 'knowledge-nav');
     if (document.body?.dataset.page === 'home') {
